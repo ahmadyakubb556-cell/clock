dd31827ba9135777f101cbf9cc46e499e272973d
ahmadyakubb556-cell <ahmadyakubb556@gmail.com>
2026-05-26 15:37:45 +0600

    Refactor ClockWise class and add rotation sound

--- clockwise.py
+++ clockwise.py
@@ -1,45 +1,72 @@
 import math
 import time
+import pygame
+
+def get_degree_of_time(title):
+        
+    module = time.localtime().tm_hour % 12
+    second = time.localtime().tm_sec
+    minute = time.localtime().tm_min
+    hour = module if module != 0 else 12
+        
+    if title == "second":
+        rotationDegree = (second * 6) 
+    elif title == "minute":
+        rotationDegree = (minute * 6) + (0.1 * second)
+    else:
+        rotationDegree = (hour * 30) + (0.5 * minute)
+            
+    return rotationDegree
+
+def rotate(cordinate,focuspoint,rotationDegree):
+            
+    angle = math.radians(rotationDegree)
+            
+    cx,cy = focuspoint
+            
+    new_cordinate = []
+            
+    for i in range(0,len(cordinate),2):
+                
+        x = cordinate[i]
+        y = cordinate[i+1]
+                
+        new_x = cx + (x - cx) * math.cos(angle) - (y - cy) * math.sin(angle)
+        new_y = cy + (x - cx) * math.sin(angle) + (y - cy) * math.cos(angle)
+                
+        new_cordinate.extend([new_x,new_y])
+                
+    return new_cordinate
+        
 
 class ClockWise:
-    def __init__(self,canvas,title,cordinate,color,width):
+    
+    def __init__(self,canvas,title,shape,cordinate,color,width):
 
         self.canvas = canvas
         self.title = title
         self.cordinate = cordinate
         self.width = width
         self.color = color
-        self.canvas.create_line(self.cordinate[0],self.cordinate[1],self.cordinate[2],self.cordinate[3],width=self.width,tags=self.title,fill=self.color)
-
-    def Active(self,rotationDegree,focuspoint):
+        self.shape = "line" if shape == "line" else "polygon"
+        self.function = getattr(self.canvas,f"create_{self.shape}")
         
-        module = time.localtime().tm_hour % 12
-        second = time.localtime().tm_sec
-        minute = time.localtime().tm_min
-        hour = module if module != 0 else 12
+        
+        self.function(*tuple(self.cordinate),width = self.width,tags = self.title,fill = self.color)
+        
+    def Active(self,focuspoint):
 
-        rotationDegree = (second * 6) if self.title == "second" else (minute * 6) + (0.1 * second) if self.title == "minute" else (hour*30) + (0.5*minute)
-        #              =    --------------------------------      -------------------------------------------------------     --------------------------
-        #              =               second                                              minute                                    hour
+        rotation_sound = "clock_sound.mp3"
 
-        angle = math.radians(rotationDegree)
-
-        x1 = self.cordinate[0]
-        y1 = self.cordinate[1]
-        x2 = self.cordinate[2]
-        y2 = self.cordinate[3]
-
-        cx = focuspoint[0]
-        cy = focuspoint[1]
-
-        rx1 = cx + (x1 - cx) * math.cos(angle) - (y1 - cy) * math.sin(angle)
-        ry1 = cy + (x1 - cx) * math.sin(angle) + (y1 - cy) * math.cos(angle)
-
-        rx2 = cx + (x2 - cx) * math.cos(angle) - (y2 - cy) * math.sin(angle)
-        ry2 = cy + (x2 - cx) * math.sin(angle) + (y2 - cy) * math.cos(angle)
-
+        pygame.mixer.init()
+        pygame.mixer.music.load(rotation_sound)
+        pygame.mixer.music.play()
+        rotationDegree = get_degree_of_time(self.title)
+        new_cordinate = tuple(rotate(self.cordinate,focuspoint,rotationDegree))
+       
         self.canvas.delete(self.title)
 
-        self.canvas.create_line(rx1,ry1,rx2,ry2,width=self.width,tags=self.title,fill=self.color)
-        self.canvas.create_oval(241,240,261,260,fill="black")
-        self.canvas.after(1000, lambda: self.Active(rotationDegree, focuspoint))
+        self.function(*new_cordinate,width = self.width,tags = self.title,fill = self.color)
+        
+        
+        self.canvas.after(1000, lambda: self.Active(focuspoint))
